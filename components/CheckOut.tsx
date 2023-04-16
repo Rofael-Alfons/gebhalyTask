5f60ddc3cbb532772f77c66bc05acfc634caf204
Rofael-Alfons <rofa.alfons7@gmail.com>
2023-04-16 22:59:53 +0200

    getStaticProps added

--- components/CheckOut.tsx
+++ components/CheckOut.tsx
@@ -4,10 +4,9 @@ import classes from "../styles/checkout.module.css";
 const CheckOut = () => {
   const dispatch = useDispatch();
   const totalPrice = useSelector((state: any) => state.products.totalPrice);
-  console.log(totalPrice);
   return (
     <div className={classes.checkout}>
-      <h2>Total Cost : {totalPrice}$</h2>
+      <h2>Total Cost : {totalPrice.toFixed(2)}$</h2>
       <button className={classes.button}>CHECKOUT</button>
     </div>
   );
